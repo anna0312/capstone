e9cc1ac5cd864f337076f9546b2fb51969a290bf
anna0312 <annabremner@gmail.com>
2018-04-20 00:40:22 -0400

    Fixed config link to heroku

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://boiling-coast-87751.herokuapp.com/',
+  production: 'https://boiling-coast-87751.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
